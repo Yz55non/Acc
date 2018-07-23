88c76245ab4e318bf056fb163945cb2c020064a4
baoyiwen <511530203@qq.com>
2018-07-23 14:50:26 +0800

    鲍艺文 2018年7月6日10:10:10 提交内容:尾部样式修改

--- src/components/handers/foot.vue
+++ src/components/handers/foot.vue
@@ -1,47 +1,66 @@
 <template>
-  <div>
-    <div class="foot">
-      <div class="foot-home flex-row">
-        <div class="foot-box-1">
+  <div class="foot">
+    <div class="foot-nav display-flex">
+      <div class="foot-code ">
+        <a href="#">
           <img src="../../assets/image/weixin/image_01.jpg" alt="微信二维码">
-          <p>学习交流微信群</p>
-        </div>
-        <div class="flex flex-row">
-          <div class="foot-box-2 ">
-            <div class="about">
-                <h3>关于</h3>
-              <ul>
-                <li><a href="">1111</a></li>
-                <li><a href="">2222</a></li>
-                <li><a href="">3333</a></li>
-                <li><a href="">4444</a></li>
-                <li><a href="">5555</a></li>
-                <li><a href="">6666</a></li>
-              </ul>
-            </div>
-          </div>
-          <div class="foot-box-3 flex ">
-            <div class="about">
-              <h3>其他链接</h3>
-              <ul>
-                <li><a href="">1111</a></li>
-                <li><a href="">2222</a></li>
-                <li><a href="">3333</a></li>
-                <li><a href="">4444</a></li>
-                <li><a href="">5555</a></li>
-                <li><a href="">6666</a></li>
-                <li><a href="">7777</a></li>
-                <li><a href="">8888</a></li>
-              </ul>
-            </div>
-          </div>
-          <div class="foot-box-2">
-            <div class="disclaimer">
-              <h3>免责声明</h3>
-              <p>网站所有内容均来自网络,若有侵权请联系本人删除.</p>
-            </div>
-          </div>
-        </div>
+        </a>
+        <p>学习交流请添加微信</p>
+      </div>
+      <div class="foot-about border-left">
+        <p>关于</p>
+        <ul>
+          <li>
+            <a href="#">网站介绍</a>
+          </li>
+          <li>
+            <a href="#">网站介绍</a>
+          </li>
+          <li>
+            <a href="#">网站介绍</a>
+          </li>
+        </ul>
+        <ul class="uls">
+          <li>
+            <a href="#">网站介绍</a>
+          </li>
+          <li>
+            <a href="#">网站介绍</a>
+          </li>
+          <li>
+            <a href="#">网站介绍</a>
+          </li>
+        </ul>
+      </div>
+      <div class="foot-conment border-left">
+        <p>漫网连接</p>
+        <ul>
+          <li>
+            <a href="#">网站介绍</a>
+          </li>
+          <li>
+            <a href="#">网站介绍</a>
+          </li>
+          <li>
+            <a href="#">网站介绍</a>
+          </li>
+        </ul>
+        <ul class="uls">
+          <li>
+            <a href="#">网站介绍</a>
+          </li>
+          <li>
+            <a href="#">网站介绍</a>
+          </li>
+          <li>
+            <a href="#">网站介绍</a>
+          </li>
+        </ul>
+      </div>
+      <div class="foot-copyright flex-1 border-left">
+        <p>
+          本项目所有页面仅供学习交流使用，如有侵权请联系本人删除
+        </p>
       </div>
     </div>
   </div>
@@ -53,6 +72,6 @@
   }
 </script>
 
-<style scoped>
-  @import "../../assets/css/foot.css";
+<style scoped lang="less">
+  @import "../../assets/foot/foot";
 </style>
