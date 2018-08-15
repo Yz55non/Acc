aac517efb44e8b07030bc9c1c8205bbf7faed2a6
baoyiwen <vincentxin@aliyun.com>
2018-08-15 17:46:22 +0800

    布局修改 2018年8月15日 17:45:56 --鲍艺文

--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -1,51 +1,34 @@
 <template>
-  <div class="main">
-    <div class="home dispaly-flex">
-      <div class="home-main">
-        <div class="index-search">
-          <a href="#">
-            <img src="../../assets/image/bukaniang.gif" height="150" width="150"/>
-          </a>
-          <div class="search-main">
-            <div class="search">
-              <input type="text" class="search-input" placeholder="请输入搜索内容">
-              <button class="search-button"></button>
-            </div>
-            <ul>
-              <li><a href="#">双星之阴阳师</a></li>
-              <li><a href="#">overload</a></li>
-              <li><a href="#">工作细胞</a></li>
-              <li><a href="#">一人之下</a></li>
-              <li><a href="#">我的青春恋爱物语果然有问题</a></li>
-            </ul>
+  <div class="index-main">
+    <div class="index-topTheme">
+      <div class="topTheme autoHeight">
+        <div class="topTheme_inner autoHeight">
+          <div class="acc-logo">
+            <a href="#" class="logo"></a>
           </div>
-          <div class="search-clode">
-              <p>
-                <a href="#">联系站长QQ</a>
-              </p>
-          </div>
-         </div>
-        <div class="home-bar">
-          <div class="bar">
-            <div class="home-bar-ch">
+          <div class="topTheme_in_search">
+            <div class="topTheme_in_search_input">
+              <div class="topTheme_search_input" style="outline: none">
+                <form action="#" ><!--method="post"-->
+                  <input type="text" class="search_input" name="keywords" autocomplete="off" placeholder="请输入关键字">
+                  <input type="submit" class="search_submit" id="searchBtn" name="searchBtn" value=" ">
+                </form>
+              </div>
               <ul>
-                <li><a href="#">首页</a></li>
-                <li><a href="#">少年</a></li>
-                <li><a href="#">少女</a></li>
-                <li><a href="#">冒险</a></li>
-                <li><a href="#">搞笑</a></li>
-                <li><a href="#">奇幻</a></li>
-                <li><a href="#">最新更新</a></li>
-                <li><a href="#">排行榜</a></li>
-                <li><a href="#">全部漫画</a></li>
-                <li><a href="#">专题</a></li>
+                <li><a href="#">双星之阴阳师</a></li>
+                <li><a href="#">overload</a></li>
+                <li><a href="#">工作细胞</a></li>
+                <li><a href="#">一人之下</a></li>
+                <li><a href="#">我的青春恋爱物语果然有问题</a></li>
               </ul>
             </div>
-            <div class="update">
-              <a href="#">上传作品</a>
+            <div class="topTheme_cloud">
+              <p>
+                <a href="#">我也不知道放什么在这里(๑ŐдŐ)b</a>
+              </p>
             </div>
           </div>
-      </div>
+        </div>
       </div>
     </div>
   </div>
