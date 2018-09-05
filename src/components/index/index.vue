f37643d9ca6358f952699d65a8bf86a6db3949ae
baoyiwen <511530203@qq.com>
2018-09-05 16:05:02 +0800

    布局修改 2018年9月5日 16:04:52 鲍艺文

--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -511,10 +511,311 @@
           </div>
         </div>
         <div class="youn_mh autoHeight">
+        <div class="con_left youn_mh_l">
+          <div class="header-public-h">
+            <h2>少年<em>漫画</em></h2>
+            <span class="h_eng">JUVENILE</span>
+            <a href="#" target="_blank" title="更多" class="more con_right">MORE+</a>
+          </div>
+          <div class="youn_cn">
+            <div class="youn_cn_b con_left">
+              <a href="#" target="_blank" title="诛仙">
+                <img src="https://images.dmzj.com/tuijian/343_336/0810zuxian.jpg" alt>
+                <span class="tip">
+                    <p>更新至第18话 逃脱</p>
+                  </span>
+              </a>
+              <p class="mh_name">
+                <a href="#" title="诛仙" target="_blank">诛仙</a>
+              </p>
+              <p>
+                <span class="con_author">作者：完美世界漫画</span>
+                <span class="con_rq con_right">人气:10086</span>
+              </p>
+              <p class="con_intro">
+                诛仙故事过去千百年。就在中原修真者即将忘记上古神剑诛仙剑的存在时，一个自小被寄养在“人妖院”的少年—…
+                <a href="#" target="_blank" title="intro" class="con_a_intro">【详情】</a>
+              </p>
+            </div>
+            <ul class="youn_con con_fir con_right">
+              <li>
+                <a href="#" target="_blank" title="极品透视小邪医">
+                  <img src="https://images.dmzj.com/tuijian/330_376/180816muguanghuanying2.jpg" alt>
+                  <span class="tip">
+                    <p>更新至第43话</p>
+                  </span>
+                </a>
+                <p class="acc-name">
+                  <a href="#" target="_blank" title="极品透视小邪医">极品透视小邪医</a>
+                </p>
+                <p class="auto">神居漫画</p>
+              </li>
+              <li>
+                <a href="#" target="_blank" title="极品透视小邪医">
+                  <img src="https://images.dmzj.com/tuijian/330_376/180816muguanghuanying2.jpg" alt>
+                  <span class="tip">
+                    <p>更新至第43话</p>
+                  </span>
+                </a>
+                <p class="acc-name">
+                  <a href="#" target="_blank" title="极品透视小邪医">极品透视小邪医</a>
+                </p>
+                <p class="auto">神居漫画</p>
+              </li>
+              <li>
+                <a href="#" target="_blank" title="极品透视小邪医">
+                  <img src="https://images.dmzj.com/tuijian/330_376/180816muguanghuanying2.jpg" alt>
+                  <span class="tip">
+                    <p>更新至第43话</p>
+                  </span>
+                </a>
+                <p class="acc-name">
+                  <a href="#" target="_blank" title="极品透视小邪医">极品透视小邪医</a>
+                </p>
+                <p class="auto">神居漫画</p>
+              </li>
+              <li>
+                <a href="#" target="_blank" title="极品透视小邪医">
+                  <img src="https://images.dmzj.com/tuijian/330_376/180816muguanghuanying2.jpg" alt>
+                  <span class="tip">
+                    <p>更新至第43话</p>
+                  </span>
+                </a>
+                <p class="acc-name">
+                  <a href="#" target="_blank" title="极品透视小邪医">极品透视小邪医</a>
+                </p>
+                <p class="auto">神居漫画</p>
+              </li>
+              <li>
+                <a href="#" target="_blank" title="极品透视小邪医">
+                  <img src="https://images.dmzj.com/tuijian/330_376/180816muguanghuanying2.jpg" alt>
+                  <span class="tip">
+                    <p>更新至第43话</p>
+                  </span>
+                </a>
+                <p class="acc-name">
+                  <a href="#" target="_blank" title="极品透视小邪医">极品透视小邪医</a>
+                </p>
+                <p class="auto">神居漫画</p>
+              </li>
+              <li>
+                <a href="#" target="_blank" title="极品透视小邪医">
+                  <img src="https://images.dmzj.com/tuijian/330_376/180816muguanghuanying2.jpg" alt>
+                  <span class="tip">
+                    <p>更新至第43话</p>
+                  </span>
+                </a>
+                <p class="acc-name">
+                  <a href="#" target="_blank" title="极品透视小邪医">极品透视小邪医</a>
+                </p>
+                <p class="auto">神居漫画</p>
+              </li>
+              <li>
+                <a href="#" target="_blank" title="极品透视小邪医">
+                  <img src="https://images.dmzj.com/tuijian/330_376/180816muguanghuanying2.jpg" alt>
+                  <span class="tip">
+                    <p>更新至第43话</p>
+                  </span>
+                </a>
+                <p class="acc-name">
+                  <a href="#" target="_blank" title="极品透视小邪医">极品透视小邪医</a>
+                </p>
+                <p class="auto">神居漫画</p>
+              </li>
+              <li>
+                <a href="#" target="_blank" title="极品透视小邪医">
+                  <img src="https://images.dmzj.com/tuijian/330_376/180816muguanghuanying2.jpg" alt>
+                  <span class="tip">
+                    <p>更新至第43话</p>
+                  </span>
+                </a>
+                <p class="acc-name">
+                  <a href="#" target="_blank" title="极品透视小邪医">极品透视小邪医</a>
+                </p>
+                <p class="auto">神居漫画</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="youn_mh_r con_right">
+          <h2>少年漫画排行榜</h2>
+          <ul class="rank_tabs tabs">
+            <li>
+              <a href="#" class="tab-option tab-option-selected">周</a>
+            </li>
+            <li>
+              <a href="#" class="tab-option">月</a>
+            </li>
+          </ul>
+          <div class="tab-content" v-if="true">
+            <ul>
+              <li class="rank_tabs_first">
+                <sub></sub>
+                <em>01</em>
+                <a href="#">
+                  <img src="https://images.dmzj.com/tuijian/330_376/180816muguanghuanying2.jpg" alt="暮光幻影">
+                </a>
+                <span class="rank_first_con">
+                    <a href="#" title="暮光幻影" target="_blank">
+                      <h3>暮光幻影</h3>
+                    </a>
+                    <p>作者：元素研究室</p>
+                    <p>更新至131话：情理之中</p>
+                  </span>
+              </li>
+              <li class="rank_tabs_li">
+                <em class="c_5">02</em>
+                <span class="rank_tabs_title">
+                    <a href="#" title="妖神记" target="_blank">妖神记</a>
+                  </span>
+                <span class="rank_tabs_sec">第174话 回归（下）</span>
+              </li>
+              <li class="rank_tabs_li">
+                <em class="c_5">03</em>
+                <span class="rank_tabs_title">
+                    <a href="#" title="妖神记" target="_blank">妖神记</a>
+                  </span>
+                <span class="rank_tabs_sec">第174话 回归（下）</span>
+              </li>
+              <li class="rank_tabs_li">
+                <em>04</em>
+                <span class="rank_tabs_title">
+                    <a href="#" title="妖神记" target="_blank">妖神记</a>
+                  </span>
+                <span class="rank_tabs_sec">第174话 回归（下）</span>
+              </li>
+              <li class="rank_tabs_li">
+                <em>05</em>
+                <span class="rank_tabs_title">
+                    <a href="#" title="妖神记" target="_blank">妖神记</a>
+                  </span>
+                <span class="rank_tabs_sec">第174话 回归（下）</span>
+              </li>
+              <li class="rank_tabs_li">
+                <em>06</em>
+                <span class="rank_tabs_title">
+                    <a href="#" title="妖神记" target="_blank">妖神记</a>
+                  </span>
+                <span class="rank_tabs_sec">第174话 回归（下）</span>
+              </li>
+              <li class="rank_tabs_li">
+                <em>07</em>
+                <span class="rank_tabs_title">
+                    <a href="#" title="妖神记" target="_blank">妖神记</a>
+                  </span>
+                <span class="rank_tabs_sec">第174话 回归（下）</span>
+              </li>
+              <li class="rank_tabs_li">
+                <em>08</em>
+                <span class="rank_tabs_title">
+                    <a href="#" title="妖神记" target="_blank">妖神记</a>
+                  </span>
+                <span class="rank_tabs_sec">第174话 回归（下）</span>
+              </li>
+              <li class="rank_tabs_li">
+                <em>09</em>
+                <span class="rank_tabs_title">
+                    <a href="#" title="妖神记" target="_blank">妖神记</a>
+                  </span>
+                <span class="rank_tabs_sec">第174话 回归（下）</span>
+              </li>
+              <li class="rank_tabs_li">
+                <em>10</em>
+                <span class="rank_tabs_title">
+                    <a href="#" title="妖神记" target="_blank">妖神记</a>
+                  </span>
+                <span class="rank_tabs_sec">第174话 回归（下）</span>
+              </li>
+            </ul>
+          </div>
+          <div class="tab-content" v-if="false">
+            <ul>
+              <li class="rank_tabs_first">
+                <sub></sub>
+                <em>01</em>
+                <a href="#">
+                  <img src="https://images.dmzj.com/tuijian/330_376/180816muguanghuanying2.jpg" alt="暮光幻影">
+                </a>
+                <span class="rank_first_con">
+                    <a href="#" title="暮光幻影" target="_blank">
+                      <h3>暮光幻影</h3>
+                    </a>
+                    <p>作者：元素研究室</p>
+                    <p>更新至131话：情理之中</p>
+                  </span>
+              </li>
+              <li class="rank_tabs_li">
+                <em class="c_5">02</em>
+                <span class="rank_tabs_title">
+                    <a href="#" title="妖神记" target="_blank">妖神记</a>
+                  </span>
+                <span class="rank_tabs_sec">第174话 回归（下）</span>
+              </li>
+              <li class="rank_tabs_li">
+                <em class="c_5">03</em>
+                <span class="rank_tabs_title">
+                    <a href="#" title="妖神记" target="_blank">妖神记</a>
+                  </span>
+                <span class="rank_tabs_sec">第174话 回归（下）</span>
+              </li>
+              <li class="rank_tabs_li">
+                <em>04</em>
+                <span class="rank_tabs_title">
+                    <a href="#" title="妖神记" target="_blank">妖神记</a>
+                  </span>
+                <span class="rank_tabs_sec">第174话 回归（下）</span>
+              </li>
+              <li class="rank_tabs_li">
+                <em>05</em>
+                <span class="rank_tabs_title">
+                    <a href="#" title="妖神记" target="_blank">妖神记</a>
+                  </span>
+                <span class="rank_tabs_sec">第174话 回归（下）</span>
+              </li>
+              <li class="rank_tabs_li">
+                <em>06</em>
+                <span class="rank_tabs_title">
+                    <a href="#" title="妖神记" target="_blank">妖神记</a>
+                  </span>
+                <span class="rank_tabs_sec">第174话 回归（下）</span>
+              </li>
+              <li class="rank_tabs_li">
+                <em>07</em>
+                <span class="rank_tabs_title">
+                    <a href="#" title="妖神记" target="_blank">妖神记</a>
+                  </span>
+                <span class="rank_tabs_sec">第174话 回归（下）</span>
+              </li>
+              <li class="rank_tabs_li">
+                <em>08</em>
+                <span class="rank_tabs_title">
+                    <a href="#" title="妖神记" target="_blank">妖神记</a>
+                  </span>
+                <span class="rank_tabs_sec">第174话 回归（下）</span>
+              </li>
+              <li class="rank_tabs_li">
+                <em>09</em>
+                <span class="rank_tabs_title">
+                    <a href="#" title="妖神记" target="_blank">妖神记</a>
+                  </span>
+                <span class="rank_tabs_sec">第174话 回归（下）</span>
+              </li>
+              <li class="rank_tabs_li">
+                <em>10</em>
+                <span class="rank_tabs_title">
+                    <a href="#" title="妖神记" target="_blank">妖神记</a>
+                  </span>
+                <span class="rank_tabs_sec">第174话 回归（下）</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+        <div class="youn_mh autoHeight">
           <div class="con_left youn_mh_l">
             <div class="header-public-h">
-              <h2>少年<em>漫画</em></h2>
-              <span class="h_eng">JUVENILE</span>
+              <h2>少女<em>漫画</em></h2>
+              <span class="h_eng">GIRL</span>
               <a href="#" target="_blank" title="更多" class="more con_right">MORE+</a>
             </div>
             <div class="youn_cn">
@@ -638,7 +939,7 @@
             </div>
           </div>
           <div class="youn_mh_r con_right">
-            <h2>少年漫画排行榜</h2>
+            <h2>少女漫画排行榜</h2>
             <ul class="rank_tabs tabs">
               <li>
                 <a href="#" class="tab-option tab-option-selected">周</a>
